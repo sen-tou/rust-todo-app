16fedac48b1e10b07a3bf1c6ea1767e87c673cb8
sen-tou <stvbyr@users.noreply.github.com>
2023-06-30 15:13:30 +0200

    read in command line args

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,25 @@
 use todo::TodoList;
 
 use crate::todo::TodoItem;
+use std::{env, error::Error};
 
 mod todo;
 
+#[derive(Debug)]
+enum Commands {
+    List,
+    Check,
+}
+
+impl Commands {
+    pub fn from_str(command: &str) -> Result<Self, Box<dyn Error>> {
+        match command {
+            "" | "l" => Ok(Self::List),
+            "c" => Ok(Self::Check),
+            _ => panic!("Command {} not found.", command),
+        }
+    }
+}
 fn main() {
     let mut item = TodoItem::new("I have to clean the bathroom");
     let new_title = format!("{} {}", item.title, "with some additions");
@@ -16,5 +32,9 @@ fn main() {
     list.add(item);
     list.add(another);
 
+    let command_args: Vec<String> = env::args().skip(1).collect();
+    let command = Commands::from_str(command_args.get(1).or(Some(&"l".to_string())).unwrap());
+
+    eprintln!("{:?}", command);
     eprintln!("{:?}", list.show());
 }
