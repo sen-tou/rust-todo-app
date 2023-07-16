1f9501ce60878f0c467ffda5d8ea34882dee020f
sen-tou <stvbyr@users.noreply.github.com>
2023-07-16 19:20:06 +0200

    add error enum

---
+++ src/error.rs
@@ -0,0 +1,15 @@
+#[derive(thiserror::Error, Debug, Clone)]
+pub enum TodoError {
+    #[error("Maximum number of {0} todos reached.")]
+    MaximumNumberOfTodosReached(usize),
+    #[error("Unable to operate on store file: {0}")]
+    StoreFileError(String),
+    #[error("Cannot find todo with index: {0}.")]
+    TodoItemNotFound(usize),
+    #[error("Some command arguments are missing. Please use h command to get help.")]
+    MissingCommandArgs,
+    #[error("There is no command with name: {0}. Please use h command to get help.")]
+    NoCommand(String),
+    #[error("Some error happened: {0}")]
+    Other(String),
+}
